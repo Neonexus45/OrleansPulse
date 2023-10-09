5919d85b29f22659c5f655da518a9d010ae8f80c
Tomaso Ethan <ethan.tomaso@etu.univ-orleans.fr>
2023-10-09 11:09:25 +0200

    ajout systeme de messages du jour

--- components/DayMessage.js
+++ components/DayMessage.js
@@ -1,15 +1,33 @@
-import React, { useEffect, useRef } from 'react';
-import { View, Text, Animated, StyleSheet } from 'react-native';
+import React, {useEffect, useRef} from 'react';
+import {Animated, StyleSheet, Text, View} from 'react-native';
 
+const getDayMessage = async (url) => {
+    const messagePromise = fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            let lines = [];
+            let i = 0;
+            data.split("\n").forEach((line) => {
+                lines[i] = line;
+                i += 1;
+            }, this);
+            return lines[1];
+        })
+        .catch(error => console.error('Error fetching file:', error));
+
+    const message = await messagePromise.json();
+    console.log(message);
+    return message;
+}
 const DayMessage = () => {
     const scrollAnim = useRef(new Animated.Value(0)).current;
-    const textWidth = 300;
-
+    const textContent = getDayMessage("https://orleanspulse.s3.eu-west-3.amazonaws.com/messages.txt");
+    const textWidth = textContent.length*8;
     useEffect(() => {
         const loop = Animated.loop(
             Animated.timing(scrollAnim, {
                 toValue: -textWidth,
-                duration: 5000,
+                duration: textWidth*10,
                 useNativeDriver: true,
             }),
             {
@@ -24,8 +42,8 @@ const DayMessage = () => {
     return (
         <View style={styles.container}>
             <Animated.View style={{ flexDirection: 'row', transform: [{ translateX: scrollAnim }] }}>
-                <Text style={styles.text} numberOfLines={1}>Message du jour : Ecoutez Duvet de bôa</Text>
-                <Text style={styles.text} numberOfLines={1}>Message du jour : Ecoutez Duvet de bôa</Text>
+                <Text numberOfLines={1} style={[styles.text, { width: textWidth }]}>{textContent}</Text>
+                <Text numberOfLines={1} style={[styles.text, { width: textWidth }]}>{textContent}</Text>
             </Animated.View>
         </View>
     );
@@ -33,11 +51,9 @@ const DayMessage = () => {
 
 const styles = StyleSheet.create({
     container: {
-        width: '100%',
-        overflow: 'hidden',
+        overflow: "hidden",
     },
     text: {
-        width: 300,
         fontSize: 12,
         fontWeight: 'bold',
     },
